ace0c44dba630a964db3f5f5cf0f0c025784e500
Rachel Yeung <rachel.cmyeung@gmail.com>
2019-12-15 16:12:04 -0800

    test modal component added, started to append restaurant information onto pop-up modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,24 +46,36 @@ function renderWeather() {
 
 
                 //----------------------------------------YELP API-----------------------------------------------
+                
+                
+                var category;
                 $("#family").on("click", function(){
                     if ($(this).is(':checked')){
                         price = "1,2,3,4"
+                        family = true;
+                        category = "kidfriendly,restaurant"
+                        console.log("family")
                     }
                 })
                 $("#budget").on("click", function(){
                     if($(this).is(':checked')){
                         price = "1,2"
+                        budget = true;
+                        category = "restaurant"
+                        console.log("budget")
                     }
                 })
                 $("#business").on("click", function(){
                     if ($(this).is(':checked')){
                         price = "2,3,4 "
+                        business = true;
+                        category = "fancy,restaurant"
+                        console.log("business")
                     }
                 })
 
-               
-
+                
+            
                 jQuery.ajaxPrefilter(function (options) {
                     if (options.crossDomain && jQuery.support.cors) {
                         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
@@ -74,6 +86,7 @@ function renderWeather() {
                     var main = response.weather[0].main
                     var lat = response.coord.lat
                     var lon = response.coord.lon
+                    
                     var type;
                     console.log(main)
                    
@@ -87,20 +100,89 @@ function renderWeather() {
                     else if (main === "Clear") {
                         type = "patio"
                     }
+//-------------------------------------------if more than one box is checked:----------------------------------------------------------------
+                    if (budget === true && family === true){
+                        price = "1,2"
+                        category = "kidfriendly,restaurant"
+                    }
+                    else if (family === true && business === true){
+                        price = "3,4"
+                        category = "kidfriendly,fancy,restaurant"
+                    }
+                    else if (budget === true && business === true){
+                        price = "1,2"
+                        category = "fancy,restaurant"
+                    }
 
                     
-                    
-                    console.log(price)
-                    
+                    console.log(type)
+                    console.log(category)
 
                     $.ajax({
-                        url: 'https://api.yelp.com/v3/businesses/search?term=' + type + '&longitude=' + lon + '&latitude=' + lat + '&price=' + price + '&open_now=true&radius=10000',
+                        url: 'https://api.yelp.com/v3/businesses/search?term=' + type + '&longitude=' + lon + '&latitude=' + lat + '&price=' + price + '&open_now=true&categories=' + category + '&radius=10000&limit=1',
                         method: "GET",
                         headers: {
                             authorization: "Bearer CyZO8Ys8yDQ-FCnqNZegGIU2FvGwOLg00MP1JtA6GLKWM2SadzcHyCA4KMt9Y9643sXFsA2bhvDY4RKLyydvPULurteiMPQKydq62F92eEKefWJnbuOanTUtAtjzXXYx"
                         }
-                    }).then(function (response) {
-                        console.log(response)
+                    }).then(function (result) {
+                        console.log(result)
+                        for (var i = 0; i < result.businesses.length; i ++){
+                            var id = result.businesses[i].id
+                            console.log(id)
+//--------------------------------YELP hours of operation ajax call-------------------------------------
+                            $.ajax({
+                                url: "https://api.yelp.com/v3/businesses/" + id,
+                                method: "GET", 
+                                headers: {
+                                    authorization: "Bearer CyZO8Ys8yDQ-FCnqNZegGIU2FvGwOLg00MP1JtA6GLKWM2SadzcHyCA4KMt9Y9643sXFsA2bhvDY4RKLyydvPULurteiMPQKydq62F92eEKefWJnbuOanTUtAtjzXXYx"
+                                }
+                            }).then(function(response){
+                                console.log(response)
+                                var restName = result.businesses[0].name
+                                var prices = result.businesses[0].price
+                                var ratings = result.businesses[0].rating
+                                var address = result.businesses[0].location.display_address[0]
+                                var address2 = result.businesses[0].location.display_address[1]
+                                var phone = result.businesses[0].display_phone
+                                for(var i = 0; i < response.hours[0].open.length; i++){
+                                    var hours = response.hours[0].open[i].start
+                                }
+                                
+
+                                $("#restaurantName").text(restName)
+                                console.log(restName)
+                                // $("#cuisine").text(response[0].categories[0].alias)
+                                // console.log(response.categories[0].alias)
+                                $("#price").text("Price: " + prices)
+                                console.log(prices)
+                                $("#rating").text("Rating: " + ratings + "/5")
+                                console.log(ratings)
+                                $("#location").text("Address: " + address + ", " +  address2)
+                                console.log(address + address2)
+                                $("#phone").text("Phone no.: " + phone)
+                                console.log(phone)
+                                $("#hours").text("Hours: " + hours)
+                            })
+                        }
+                        
+                        
+
+
+         
+                        // $.ajax({
+                        //     url: "https://api.yelp.com/v3/businesses/" + id,
+                        //     method: "GET", 
+                        //     headers: {
+                        //         authorization: "Bearer CyZO8Ys8yDQ-FCnqNZegGIU2FvGwOLg00MP1JtA6GLKWM2SadzcHyCA4KMt9Y9643sXFsA2bhvDY4RKLyydvPULurteiMPQKydq62F92eEKefWJnbuOanTUtAtjzXXYx"
+                        //     }
+                        // }).then(function(response){
+                        //     console.log(response)
+                        // })
+
+
+
+
+
                     })
 
                 })
@@ -116,3 +198,10 @@ renderWeather();
 
 
 
+$("#showModal").click(function() {
+    $(".modal").addClass("is-active");  
+  });
+  
+  $(".modal-close").click(function() {
+     $(".modal").removeClass("is-active");
+  });
