3ac633de975ea2977db4ea1ad0201ea4ec42d88b
oren amema <orenamema@gmail.com>
2019-12-16 23:03:11 -0800

    Adding the last page, saving progress

--- assets/js/businessPage.js
+++ assets/js/businessPage.js
@@ -2,31 +2,42 @@ data = JSON.parse(localStorage.getItem("businessData"));
 lat = data.coordinates.latitude;
 lon = data.coordinates.longitude;
 businessImage = data.image_url;
-console.log(businessImage);
 businessCusine = data.categories[0].title;
-console.log(businessCusine);
 businessPrice = data.price;
-console.log(businessPrice);
 businessRating = data.rating;
-console.log(businessRating);
 businessLocation = `${data.location.address1}, 
                         ${data.location.city}, 
                         ${data.location.state} 
                         ${data.location.zip_code}`;
-console.log(businessLocation);
 businessPhone = data.display_phone;
-console.log(businessPhone);
+businessName = data.name;
+
+//  https://www.spacearchive.info/military.htm
+matcher = {'0000':'Midnight' ,'0100':'1:00 a.m.' ,'0200':'2:00 a.m.' ,'0300':'3:00 a.m.' 
+        ,'0400':'4:00 a.m.' ,'0500':'5:00 a.m.' ,'0600':'6:00 a.m.' ,'0700':'7:00 a.m.' 
+        ,'0800':'8:00 a.m.' ,'0900':'9:00 a.m.' ,'1000':'10:00 a.m.' ,'1100':'11:00 a.m.' 
+        ,'1200':'Noon' ,'1300':'1:00 p.m.' ,'1400':'2:00 p.m.' ,'1500':'3:00 p.m.' 
+        ,'1600':'4:00 p.m.' ,'1700':'5:00 p.m.' ,'1800':'6:00 p.m.' ,'1900':'7:00 p.m.' 
+        ,'2000':'8:00 p.m.' ,'2100':'9:00 p.m.' ,'2200':'10:00 p.m.' ,'2300':'11:00 p.m.'};
 
 // console.log($("#cuisineP"));
 
 function change(){
     // Update ids 
+    $("#businessP").text(businessName);
     $("#business-image").attr("src", businessImage);
-    $("#cuisineP").text(`Cuisine: ${businessCusine}`);
-    $("#priceP").text(`Price: ${businessPrice}`);
-    $("#ratingP").text(`Rating: ${businessRating}/5`);
-    $("#locationP").text(`Location: ${businessLocation}`);
-    $("#phoneP").text(`Phone number: ${businessPhone}`);
+    $("#cuisineP").text(`${businessCusine}`);
+    $("#priceP").text(`${businessPrice}`);
+    $("#ratingP").text(`${businessRating}/5`);
+    $("#locationP").text(`${businessLocation}`);
+    $("#phoneP").text(`${businessPhone}`);
+    $("#mondayP").text(`${data.hours[0].open[0].start} - ${data.hours[0].open[0].end}`);
+    $("#tuesdayP").text(`${data.hours[0].open[1].start} - ${data.hours[0].open[1].end}`);
+    $("#wednesdayP").text(`${data.hours[0].open[2].start} - ${data.hours[0].open[2].end}`);
+    $("#thursdayP").text(`${data.hours[0].open[3].start} - ${data.hours[0].open[3].end}`);
+    $("#fridayP").text(`${data.hours[0].open[4].start} - ${data.hours[0].open[4].end}`);
+    $("#saturdayP").text(`${data.hours[0].open[5].start} - ${data.hours[0].open[5].end}`);
+    $("#sundayP").text(`${data.hours[0].open[6].start} - ${data.hours[0].open[6].end}`);
 }
 change();
 // update map
@@ -41,5 +52,6 @@ function myMap() {
     
     marker.setMap(map);
 }
+
 
 
