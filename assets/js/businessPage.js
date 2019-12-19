b3ea823cd093121c16571288f1540f19ef98e391
Rachel Yeung <rachel.cmyeung@gmail.com>
2019-12-18 16:36:43 -0800

    cleaned up code

--- assets/js/businessPage.js
+++ assets/js/businessPage.js
@@ -11,100 +11,74 @@ businessLocation = `${data.location.address1},
                         ${data.location.zip_code}`;
 businessPhone = data.display_phone;
 businessName = data.name;
+console.log(data)
+function change() {
+  $("#businessP").text(businessName);
+  $("#business-image").attr("src", businessImage);
+  $("#cuisineP").text(`${businessCusine}`);
+  $("#priceP").text(`${businessPrice}`);
+  $("#ratingP").text(`${businessRating}/5`);
+  $("#locationP").text(`${businessLocation}`);
+  $("#phoneP").text(`${businessPhone}`);
 
-//  https://www.spacearchive.info/military.htm
-matcher = {'0000':'Midnight' ,'0100':'1:00 a.m.' ,'0200':'2:00 a.m.' ,'0300':'3:00 a.m.' 
-        ,'0400':'4:00 a.m.' ,'0500':'5:00 a.m.' ,'0600':'6:00 a.m.' ,'0700':'7:00 a.m.' 
-        ,'0800':'8:00 a.m.' ,'0900':'9:00 a.m.' ,'1000':'10:00 a.m.' ,'1100':'11:00 a.m.' 
-        ,'1200':'Noon' ,'1300':'1:00 p.m.' ,'1400':'2:00 p.m.' ,'1500':'3:00 p.m.' 
-        ,'1600':'4:00 p.m.' ,'1700':'5:00 p.m.' ,'1800':'6:00 p.m.' ,'1900':'7:00 p.m.' 
-        ,'2000':'8:00 p.m.' ,'2100':'9:00 p.m.' ,'2200':'10:00 p.m.' ,'2300':'11:00 p.m.'};
+  //business hours
+  for (var i = 0; i < data.hours[0].open.length; i++) {
+    var day = data.hours[0].open[i].day
+    console.log(day)
+    var dayWeek;
+    if (day === 0) {
+      dayWeek = "Sunday"
+    }
+    else if (day === 1) {
+      dayWeek = "Monday"
+    }
+    else if (day === 2) {
+      dayWeek = "Tuesday"
+    }
+    else if (day === 3) {
+      dayWeek = "Wednesday"
+    }
+    else if (day === 4) {
+      dayWeek = "Thursday"
+    }
+    else if (day === 5) {
+      dayWeek = "Friday"
+    }
+    else if (day === 6) {
+      dayWeek = "Saturday"
+    }
+    if (data.hours[0].open[i] === data.hours[0].open[i + 1]) {
+      var start = data.hours[0].open[i].start
+      var end = data.hours[0].open[i].end
+      var start1 = data.hours[0].open[i + 1].start
+      var end1 = data.hours[0].open[i + 1].start
+      var hourDiv = $("<div>" + dayWeek + ": " + start + "-" + end + "," + start1 + "-" + end1 + "</div>")
+      var hoursDiv = $("<div>       " + start1 + "-" + end1 + "</div")
+      $("#hours").append(hourDiv)
+      $("#hours").append(hoursDiv)
+    }
+    else {
 
-// console.log($("#cuisineP"));
+      var start = data.hours[0].open[i].start
+      var end = data.hours[0].open[i].end
+      var hourDiv = $("<div>" + dayWeek + ": " + start + "-" + end + "</div>")
+      $("#hours").append(hourDiv)
+    }
 
-function change(){
-    // Update ids 
-    $("#businessP").text(businessName);
-    $("#business-image").attr("src", businessImage);
-    $("#cuisineP").text(`${businessCusine}`);
-    $("#priceP").text(`${businessPrice}`);
-    $("#ratingP").text(`${businessRating}/5`);
-    $("#locationP").text(`${businessLocation}`);
-    $("#phoneP").text(`${businessPhone}`);
-    // $("#mondayP").text(`${data.hours[0].open[0].start} - ${data.hours[0].open[0].end}`);
-    // $("#tuesdayP").text(`${data.hours[0].open[1].start} - ${data.hours[0].open[1].end}`);
-    // $("#wednesdayP").text(`${data.hours[0].open[2].start} - ${data.hours[0].open[2].end}`);
-    // $("#thursdayP").text(`${data.hours[0].open[3].start} - ${data.hours[0].open[3].end}`);
-    // $("#fridayP").text(`${data.hours[0].open[4].start} - ${data.hours[0].open[4].end}`);
-    // $("#saturdayP").text(`${data.hours[0].open[5].start} - ${data.hours[0].open[5].end}`);
-    // $("#sundayP").text(`${data.hours[0].open[6].start} - ${data.hours[0].open[6].end}`);
-
-    for(var i = 0; i < data.hours[0].open.length; i++){
-      //             // var hours = response.hours[0].open[i].start
-      //             // var hourDiv = $("<div>" + hours + "</div>")
-      //             // $("#hours").append(hourDiv)
-      //             // $("#hours").text("Hours: " + hours)
-      //             // console.log(hours)
-                  var day = data.hours[0].open[i].day
-                  console.log(day)
-                  var dayWeek;
-                  if ( day === 0){
-                      dayWeek = "Sunday"
-                  }
-                  else if( day === 1){
-                      dayWeek = "Monday"
-                  }
-                  else if (day === 2){
-                      dayWeek = "Tuesday"
-                  }
-                  else if (day === 3){
-                      dayWeek = "Wednesday"
-                  }
-                  else if (day === 4){
-                      dayWeek = "Thursday"
-                  }
-                  else if (day === 5){
-                      dayWeek = "Friday"
-                  }
-                  else if (day === 6){
-                      dayWeek = "Saturday"
-                  }
-                  if(data.hours[0].open[i] === data.hours[0].open[i+1]){
-                      var start = data.hours[0].open[i].start
-                      var end = data.hours[0].open[i].end
-                      var start1 = data.hours[0].open[i+1].start
-                      var end1 = data.hours[0].open[i+1].start
-                      var hourDiv = $("<div>" + dayWeek +": " + start + "-" + end + "," + start1 + "-" + end1 + "</div>")
-                      var hoursDiv = $("<div>       " + start1 + "-" + end1 + "</div" )
-                      console.log(start,end,start1,end1)
-                      $("#hours").append(hourDiv)
-                    console.log("hello")
-                      $("#hours").append(hoursDiv)
-                  }
-                  else{
-                    
-                      var start = data.hours[0].open[i].start
-                      var end = data.hours[0].open[i].end
-                      var hourDiv = $("<div>" +dayWeek + ": " + start + "-" + end + "</div>")
-                      $("#hours").append(hourDiv)
-                      console.log(start,end)
-                      console.log("hello")
-                  }
-          
-              }
+  }
 }
 change();
 // update map
 function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(lat,lon),
-      zoom:15,
-      disableDefaultUI: true
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    var marker = new google.maps.Marker({position: mapProp.center});
-    
-    marker.setMap(map);
+  var mapProp = {
+    center: new google.maps.LatLng(lat, lon),
+    zoom: 15,
+    disableDefaultUI: true
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  var marker = new google.maps.Marker({ position: mapProp.center });
+
+  marker.setMap(map);
 }
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
 var weatherContent = $("#weather-content");
-// jQuery.ajaxPrefilter(function (options) {
-//     if (options.crossDomain && jQuery.support.cors) {
-//       options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-//     }
-//   });
-
 
 function renderWeather() {
 
@@ -45,37 +39,30 @@ function renderWeather() {
                 weatherContent.append(windSpeed);
 
 
-                //----------------------------------------YELP API-----------------------------------------------
-                
-                
+                // checkboxes
                 var category;
-                $("#family").on("click", function(){
-                    if ($(this).is(':checked')){
+                $("#family").on("click", function () {
+                    if ($(this).is(':checked')) {
                         price = "1,2,3,4"
                         family = true;
                         category = "kidfriendly,restaurant"
-                        console.log("family")
                     }
                 })
-                $("#budget").on("click", function(){
-                    if($(this).is(':checked')){
+                $("#budget").on("click", function () {
+                    if ($(this).is(':checked')) {
                         price = "1,2"
                         budget = true;
                         category = "restaurant"
-                        console.log("budget")
                     }
                 })
-                $("#business").on("click", function(){
-                    if ($(this).is(':checked')){
+                $("#business").on("click", function () {
+                    if ($(this).is(':checked')) {
                         price = "2,3,4 "
                         business = true;
                         category = "fancy,restaurant"
-                        console.log("business")
                     }
                 })
 
-                
-            
                 jQuery.ajaxPrefilter(function (options) {
                     if (options.crossDomain && jQuery.support.cors) {
                         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
@@ -83,17 +70,15 @@ function renderWeather() {
                 });
                 $("#restaurants").on("click", function () {
                     event.preventDefault()
-                    $("#body").attr("style","background-image: none");
+                    $("#body").attr("style", "background-image: none");
                     $("#find-restaurants-div").attr("style", "display: none");
-                    $("#filter-div").attr("style","display: none");
+                    $("#filter-div").attr("style", "display: none");
 
                     var main = response.weather[0].main
                     var lat = response.coord.lat
                     var lon = response.coord.lon
-                    
+
                     var type;
-                    console.log(main)
-                   
 
                     if (main === "Clouds" || main === "Rain") {
                         type = "soup"
@@ -104,27 +89,23 @@ function renderWeather() {
                     else if (main === "Clear") {
                         type = "patio"
                     }
-//-------------------------------------------if more than one box is checked:----------------------------------------------------------------
-                    if (budget === true && family === true){
+                    //if more than one box is checked
+                    if (budget === true && family === true) {
                         price = "1,2"
                         category = "kidfriendly,restaurant"
                     }
-                    else if (family === true && business === true){
+                    else if (family === true && business === true) {
                         price = "3,4"
                         category = "kidfriendly,fancy,restaurant"
                     }
-                    else if (budget === true && business === true){
+                    else if (budget === true && business === true) {
                         price = "1,2"
                         category = "fancy,restaurant"
                     }
-
-                    
-                    console.log(type)
-                    console.log(category)
-
+                    //First Yelp API call
                     $.ajax({
-                        url: 'https://api.yelp.com/v3/businesses/search?term=' + type + '&longitude=' + lon + '&latitude=' + lat + '&price=' + 
-                        price + '&open_now=true&categories=' + category + '&radius=10000&limit=12',
+                        url: 'https://api.yelp.com/v3/businesses/search?term=' + type + '&longitude=' + lon + '&latitude=' + lat + '&price=' +
+                            price + '&open_now=true&categories=' + category + '&radius=10000&limit=48',
                         method: "GET",
                         headers: {
                             authorization: "Bearer CyZO8Ys8yDQ-FCnqNZegGIU2FvGwOLg00MP1JtA6GLKWM2SadzcHyCA4KMt9Y9643sXFsA2bhvDY4RKLyydvPULurteiMPQKydq62F92eEKefWJnbuOanTUtAtjzXXYx"
@@ -132,102 +113,6 @@ function renderWeather() {
                     }).then(function (result) {
                         console.log(result)
                         runPackery(result.businesses);
-
-                        // for (var i = 0; i < result.businesses.length; i ++){
-                        //     var id = result.businesses[i].id
-                        //     console.log(id)
-                        //     //--------------------------------YELP hours of operation ajax call-------------------------------------
-                        //     $.ajax({
-                        //         url: "https://api.yelp.com/v3/businesses/" + id,
-                        //         method: "GET", 
-                        //         headers: {
-                        //             authorization: "Bearer CyZO8Ys8yDQ-FCnqNZegGIU2FvGwOLg00MP1JtA6GLKWM2SadzcHyCA4KMt9Y9643sXFsA2bhvDY4RKLyydvPULurteiMPQKydq62F92eEKefWJnbuOanTUtAtjzXXYx"
-                        //         }
-                        //     }).then(function(response){
-                        //         console.log(response)
-                        //         var restName = result.businesses[0].name
-                        //         var prices = result.businesses[0].price
-                        //         var ratings = result.businesses[0].rating
-                        //         var address = result.businesses[0].location.display_address[0]
-                        //         var address2 = result.businesses[0].location.display_address[1]
-                        //         var phone = result.businesses[0].display_phone
-                        //         // var sunday = response.hours[0].open[0].start
-                        //         // var sunday1 = response.hours[0].open[0].end
-                        //         for(var i = 0; i < response.hours[0].open.length; i++){
-                        //             // var hours = response.hours[0].open[i].start
-                        //             // var hourDiv = $("<div>" + hours + "</div>")
-                        //             // $("#hours").append(hourDiv)
-                        //             // $("#hours").text("Hours: " + hours)
-                        //             // console.log(hours)
-                        //             var day = response.hours[0].open[i].day
-                        //             console.log(day)
-                        //             var dayWeek;
-                        //             if ( day === 0){
-                        //                 dayWeek = "Sunday"
-                        //             }
-                        //             else if( day === 1){
-                        //                 dayWeek = "Monday"
-                        //             }
-                        //             else if (day === 2){
-                        //                 dayWeek = "Tuesday"
-                        //             }
-                        //             else if (day === 3){
-                        //                 dayWeek = "Wednesday"
-                        //             }
-                        //             else if (day === 4){
-                        //                 dayWeek = "Thursday"
-                        //             }
-                        //             else if (day === 5){
-                        //                 dayWeek = "Friday"
-                        //             }
-                        //             else if (day === 6){
-                        //                 dayWeek = "Saturday"
-                        //             }
-                        //             if(response.hours[0].open[i] === response.hours[0].open[i+1]){
-                        //                 var start = response.hours[0].open[i].start
-                        //                 var end = response.hours[0].open[i].end
-                        //                 var start1 = response.hours[0].open[i+1].start
-                        //                 var end1 = response.hours[0].open[i+1].start
-                        //                 var hourDiv = $("<div>" + dayWeek +": " + start + "-" + end + "</div>")
-                        //                 var hoursDiv = $("<div>" + start1 + "-" + end1 + "</div" )
-                        //                 console.log(start,end,start1,end1)
-                        //                 $("#hours").append(hourDiv, hoursDiv)
-                        //             }
-                        //             else{
-                        //                 var start = response.hours[0].open[i].start
-                        //                 var end = response.hours[0].open[i].end
-                        //                 var hourDiv = $("<div>" +dayWeek + ": " + start + "-" + end + "</div>")
-                        //                 $("#hours").append(hourDiv)
-                        //                 console.log(start,end)
-                        //             }
-                            
-                        //         }
-                                
-
-                        //         $("#restaurantName").text(restName)
-                        //         console.log(restName)
-                        //         // $("#cuisine").text(response[0].categories[0].alias)
-                        //         // console.log(response.categories[0].alias)
-                        //         $("#price").html("<strong>Price: </strong>" + prices)
-                        //         console.log(prices)
-                        //         $("#rating").html("<strong>Rating: </strong>" + ratings + "/5")
-                        //         console.log(ratings)
-                        //         $("#location").html("<strong>Address: </strong>" + address + ", " +  address2)
-                        //         console.log(address + address2)
-                        //         $("#phone").html("<strong>Phone no.: </strong>" + phone)
-                        //         console.log(phone)
-                        //         var img = $("<img>")
-                        //         img.attr("src", result.businesses[0].image_url)
-                        //         img.attr("style", "height: 150px; width: 150px; position: absolute; right: 30px;")
-                        //         $("#img").append(img)
-                        //         // var sundayDiv = $("<div>" + sunday + "- " + sunday1 + "</div>")
-                        //         // $("#hours").append(sundayDiv)
-                        //         // console.log(sunday + sunday1)
-                                
-                        //     })
-                        // }
-                        
-
                     })
 
                 })
@@ -239,9 +124,9 @@ function renderWeather() {
     }
 }
 
-function runPackery(arr){
+function runPackery(arr) {
     var grid = $(".grid");
-    for(var i = 0; i < arr.length; i++){
+    for (var i = 0; i < arr.length; i++) {
         var gridItem = $("<div>");
         gridItem.addClass("grid-item");
         grid.append(gridItem);
@@ -251,47 +136,41 @@ function runPackery(arr){
         image.addClass("restaurant-img");
         image.attr("id", arr[i].id);
         gridItem.append(image);
-        $("#"+arr[i].id).click(function() {
-            showModal(this.id); 
+        $("#" + arr[i].id).click(function () {
+            showModal(this.id);
         });
-        
+
     }
 
-    var $grid =     $('.grid').packery({
+    var $grid = $('.grid').packery({
         itemSelector: '.grid-item'
     });
 
-    $grid.imagesLoaded().progress( function() {
-    $grid.packery();
-});
+    $grid.imagesLoaded().progress(function () {
+        $grid.packery();
+    });
 }
 
 renderWeather();
 
-
-function showModal(id){
+//Second Yelp API call 
+function showModal(id) {
     $.ajax({
         url: "https://api.yelp.com/v3/businesses/" + id,
-        method: "GET", 
+        method: "GET",
         headers: {
             authorization: "Bearer CyZO8Ys8yDQ-FCnqNZegGIU2FvGwOLg00MP1JtA6GLKWM2SadzcHyCA4KMt9Y9643sXFsA2bhvDY4RKLyydvPULurteiMPQKydq62F92eEKefWJnbuOanTUtAtjzXXYx"
         }
-    }).then(function(response){ 
-            console.log(response)
-            localStorage.setItem("businessData", JSON.stringify(response));
-            $(".modal").addClass("is-active");
-           
-            window.open("businessPage.html", '_blank');
-            
-    });  
+    }).then(function (response) {
+        console.log(response)
+        localStorage.setItem("businessData", JSON.stringify(response));
+        $(".modal").addClass("is-active");
+
+        window.open("businessPage.html", '_blank');
+
+    });
 }
 
 
 
-// $("#showModal").click(function() {
-//     $(".modal").addClass("is-active");  
-//   });
-  
-  $(".modal-close").click(function() {
-     $(".modal").removeClass("is-active");
-  });
+
